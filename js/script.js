1d8ea56b5731f13e565bbe86b89d03f64b9f3128
mohit <mohitsaroha03@gmail.com>
2025-01-08 21:50:35 +0530

    index

--- js/script.js
+++ js/script.js
@@ -115,24 +115,24 @@
 // }); 
 
 
-// let mybutton = document.getElementById("btn-back-to-top");
-// window.onscroll = function () {
-//   scrollFunction();
-// };
-// function scrollFunction() {
-//   if (
-//     document.body.scrollTop > 20 ||
-//     document.documentElement.scrollTop > 20
-//   ) {
-//     mybutton.style.display = "block";
-//   } else {
-//     mybutton.style.display = "none";
-//   }
-// }
-// mybutton.addEventListener("click",function(){
-//   document.body.scrollTop = 0;
-//   document.documentElement.scrollTop = 0;
-// });
+let mybutton = document.getElementById("btn-back-to-top");
+window.onscroll = function () {
+  scrollFunction();
+};
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+mybutton.addEventListener("click",function(){
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+});
 
 
 const nameInput = document.getElementById('name');
